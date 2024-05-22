ee08a7c090e48c35f62e9dd48ea572ba6ea5078b
Obiorachibuike <obiorachibuike22@gaiml.com>
2024-05-22 13:48:47 +0100

    Firstcommit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Quiz from './components/Quize'
+
+function App() {
+ 
+
+  return (
+    <>
+     <Quiz />
+    </>
+  )
+}
+
+export default App
